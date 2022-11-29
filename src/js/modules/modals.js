a4507b26802624d6d6a8c4c4ffdc6ad4504a3e3f
SerG <serg.arnisha@gmail.com>
2022-11-29 11:41:30 +0100

    fix bugs

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -12,7 +12,8 @@ const modals = () => {
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
       close = document.querySelector(closeSelector),
-      windows = document.querySelectorAll("[data-modal]");
+      windows = document.querySelectorAll("[data-modal]"),
+      scroll = calcScroll();
 
     trigger.forEach((item) => {
       item.addEventListener("click", (e) => {
@@ -24,6 +25,8 @@ const modals = () => {
         });
         modal.style.display = "block";
         document.body.style.overflow = "hidden";
+
+        document.body.style.marginRight = `${scroll}px`;
       });
     });
 
@@ -34,6 +37,8 @@ const modals = () => {
 
       modal.style.display = "none";
       document.body.style.overflow = "";
+
+      document.body.style.marginRight = `0px`;
     });
 
     // close => click for empty place
@@ -45,6 +50,8 @@ const modals = () => {
         });
         modal.style.display = "none";
         document.body.style.overflow = "";
+
+        document.body.style.marginRight = `0px`;
       }
     });
     // --> close
@@ -55,6 +62,24 @@ const modals = () => {
       document.querySelector(selector).style.display = "flex";
       document.body.style.overflow = "";
     }, time);
+  }
+
+  /* fix bug for jump window when open modal */
+  function calcScroll() {
+    let div = document.createElement("div");
+
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.style.overflow = "scroll";
+    div.style.visibility = "hidden";
+
+    document.body.appendChild(div);
+
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+
+    div.remove();
+
+    return scrollWidth;
   }
 
   bindModal(
