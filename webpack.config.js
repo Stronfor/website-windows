906b8c8d0bd7e70c94c43c9e047758151e817814
SerG <serg.arnisha@gmail.com>
2022-12-04 20:25:06 +0100

    production

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development", // production
+  mode: "production", // production
   entry: "./src/js/main.js",
   output: {
     filename: "bundl.js",
     path: path.resolve(__dirname, "dist"),
   },
   watch: true,
+  devtool: "source-map",
 };
